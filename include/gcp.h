cb17f67578f9f88624e97c8f65e2f0e5acf5fbee
Jörg Kussmann <joerg.kussmann@cup.uni-muenchen.de>
2023-01-23 17:53:10 +0100

    added c/c++ header file added cxx-api test

---
+++ include/gcp.h
@@ -0,0 +1,43 @@
+/* This file is part of mctc-gcp.
+* SPDX-Identifier: LGPL-3.0-or-later
+*
+* mctc-gcp is free software: you can redistribute it and/or modify it under
+* the terms of the Lesser GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* mctc-gcp is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* Lesser GNU General Public License for more details.
+*
+* You should have received a copy of the Lesser GNU General Public License
+* along with mctc-gcp.  If not, see <https://www.gnu.org/licenses/>.
+**/
+
+#ifndef GCP_H
+#define GCP_H
+
+#ifdef __cplusplus
+#define GCP_API_ENTRY extern "C"
+#else
+#define GCP_API_ENTRY extern
+#endif
+
+GCP_API_ENTRY void c_gcp_call(int* n,             // No. of atoms
+                              double* xyz,        // coordinates (3,n) in Bohr
+                              double* lat,        // lattice matrix
+                              int* iz,            // element numbers
+                              double* gcp_e,      // gcp energy
+                              double* gcp_g,      // gcp gradient
+                              double* gcp_glat,   // gcp lattice gradient
+                              bool* dograd,       // flag: evaluate gradient
+                              bool* dohess,       // flag: evaluate hessian (file: gcp_hessian)
+                              bool* pbc,          // flag: periodic boundary conditions
+                              const char* method, // method name
+                              bool* echo,         // flag: verbose output
+                              bool* parfile       // flag: print extended parameter file (gcp.param)
+                              );
+
+#endif
+
