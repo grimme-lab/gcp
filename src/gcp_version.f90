c9e2bf3f0db73f2d578781f014cb5e588e9553f7
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2026-08-17 10:30:30 +0200

    Bump to version 2.4.0 (#32)

--- src/gcp_version.f90
+++ src/gcp_version.f90
@@ -24,10 +24,10 @@ module gcp_version
 
 
    !> String representation of the mctc-gcp version
-   character(len=*), parameter :: gcp_version_string = "2.3.2"
+   character(len=*), parameter :: gcp_version_string = "2.4.0"
 
    !> Numeric representation of the mctc-gcp version
-   integer, parameter :: gcp_version_compact(3) = [2, 3, 2]
+   integer, parameter :: gcp_version_compact(3) = [2, 4, 0]
 
 
 contains
